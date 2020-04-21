94be5bfba61b2f4b203e0f69d0417a6579af5b88
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-21 15:10:42 +0200

    esercizio mail array + for

---
+++ mail/main.js
@@ -0,0 +1,9 @@
+//creare una lista di email= creare un array che constiene n indirizzi email
+var lista_email = ['vincentvangogh@fakemail.com', 'andywarhol@fakemail.com', 'salvadordali@fakemail.com', 'picasso@fakemail.com', 'claudemonet@fakemail.com', 'raffaellosanzio@fakemail.com', 'gauguinpaul@fakemail.com', 'leonardodavinci@fakemail.com', 'renoir@fakemail.com', 'paulcezanne@fakemail.com'];
+console.log(lista_email);
+for(var i = 0; i < lista_email.lenght; i++);
+console.log(lista_email[i]);
+//Chiedi all’utente la sua email
+var email_utente = prompt('Inserisci la tua email, grazie!');
+console.log(email_utente);
+//stampa un messaggio appropriato
