e509ee870e24aba6d69116abbc15cc26853a1b10
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-21 16:09:59 +0200

    correzioni

---
+++ dadi/main.js
@@ -0,0 +1,3 @@
+//chiedi di inserire un numero intero tra 1 e 6
+var numero = prompt('Inserisci un numero intero da 1 a 6!');
+console.log('numero inserito: ' + numero);
--- mail/main.js
+++ mail/main.js
@@ -1,13 +1,18 @@
+var email_utente = prompt('Inserisci la tua email, grazie!');
+console.log(email_utente);
 //creare una lista di email= creare un array che constiene n indirizzi email
 var lista_email = ['vincentvangogh@fakemail.com', 'andywarhol@fakemail.com', 'salvadordali@fakemail.com', 'picasso@fakemail.com', 'claudemonet@fakemail.com', 'raffaellosanzio@fakemail.com', 'gauguinpaul@fakemail.com', 'leonardodavinci@fakemail.com', 'renoir@fakemail.com', 'paulcezanne@fakemail.com'];
 console.log(lista_email);
-for(var i = 0; i < lista_email.lenght; i++);
-console.log(lista_email[i]);
-//Chiedi all’utente la sua email
-var email_utente = prompt('Inserisci la tua email, grazie!');
-console.log(email_utente);
-//verifica che l'email sia presente bela lista
-var n = lista_email.includes(email_utente);
-console.log(n);
-//stampa un messaggio appropriato se l'email è presente o meno
-if()
+
+//controllo
+var controllo= false;
+for(var i = 0; i < lista_email.lenght; i++) {
+    if (email_utente == lista_email[i])  {
+        controllo=true;
+    }
+}
+if (controllo) {
+    console.log('Sei nella lista');
+} else {
+    console.log('Non sei nella lista');
+}
